88a7bc92f90da36c1a0d7add38ca1fbca035468e
Ikawari <lnmanese@student.hau.edu.ph>
2024-03-15 11:45:10 +0800

    udpate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Message from './Message.tsx';
 import Message2 from './Message2.tsx';
+import ListGroup from './components/ListGroup.tsx';
 
 function App() {
-  // return <div><Message /></div>
-  return <div><Message2 /></div>
+  return <>
+  <div><Message /></div>
+  <div> <ListGroup /></div>
+  <div><Message2 /></div>
+
+  </>
 }
 
 export default App
